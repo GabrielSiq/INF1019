1c127474782b9356b257eb899618ee177a98f664
gabrielsiq <gabrielsiq@msn.com>
2016-10-13 09:29:26 -0300

    Added integrity check for commands and program; time display

--- T1/PriorityInterpreter.c
+++ T1/PriorityInterpreter.c
@@ -2,17 +2,50 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
 
 #define PROGRAM_LIMIT 50// Suporta uma entrada de até 50 programas com sintaxe VÁLIDA
 #define CHAR_LIMIT 50
 
-
-//TODO: FAZER CHECAGEM DE INTEGRIDADE EM FUNÇÕES SEPARADAS
-//TODO: FAZER CHECAGEM DE EXISTÊNCIA DOS PROGRAMAS PARA TESTE
 //TODO: PASSAR AS COISAS POR MEMÓRIA COMPARTILHADA
 //TODO: SINCRONIZAR ESCALONADOR E INTERPRETADOR POR SEMÁFORO OU SIGNAL
 
-//TODO:
+//TODO: PASSAR AS FUNÇÕES AUXILIARES PARA UM UTIL.H
+
+void printTime(){
+	time_t rawtime;
+  	struct tm * timeinfo;
+
+  	time ( &rawtime );
+  	timeinfo = localtime ( &rawtime );
+  	printf ( "[%d:%d:%d] ", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+}
+
+// Checa a integridade do comando de entrada
+int integrityCheck(char * command, char * program, char * priority){
+	char priorityIntegrity[9];
+	int priorityValue;
+	struct stat programCheck;
+
+	strncpy(priorityIntegrity, priority, 9);
+	priorityIntegrity[9] = 0;
+	if(strcmp(command, "exec") != 0){
+		return -1;
+	}
+	if(strcmp(priorityIntegrity, "priority=") != 0){
+		return -2;
+	}
+	priorityValue = (int) priority[9] - '0';
+	if(priorityValue > 7 || priorityValue < 0){
+		return -3;
+	}
+	if(!(stat(program, &programCheck) == 0 && programCheck.st_mode & S_IXUSR)) {
+		return -4;
+	}
+	return priorityValue;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -21,16 +54,19 @@ int main(int argc, char const *argv[])
 	int programCount=0, i, priorityList[PROGRAM_LIMIT], priorityValue;
 	FILE *input, *output;
 
+	printTime();
 	printf("Interpretador de Prioridade:\n");
 	// A entrada e a saída serão feitas por arquivo,
 	// de modo a facilitar a avaliação e teste.
 	input = fopen("input_priority.txt", "r");
 	if(input == NULL){
+		printTime();
 		printf("Erro na abertura do arquivo de entrada.\n");
 		exit(1);
 	}
 	output = fopen("output_priority.txt", "w");
 	if(output == NULL){
+		printTime();
 		printf("Erro na abertura do arquivo de saída.\n");
 		exit(1);
 	}
@@ -46,26 +82,30 @@ int main(int argc, char const *argv[])
 	//Leitura dos comandos
 	while(fscanf(input, "%s %s %s", command, program, priority)  == 3){
 		// separa a prioridade
-		strncpy(priorityIntegrity, priority, 9);
-		priorityIntegrity[9] = 0;
+		priorityValue = integrityCheck(command, program, priority);
 
-		if(strcmp(command, "exec") != 0){
+		if(priorityValue == -1){
+			printTime();
 			printf("O comando %s na linha %d não é reconhecido como um comando válido. A linha será ignorada.\n", command, i);
 		}
-		else if(strcmp(priorityIntegrity, "priority=") != 0){
+		else if(priorityValue == -2){
+			printTime();
 			printf("O argumento %s na linha %d não é reconhecido como um argumento de prioridade válido. A linha será ignorada.\n", priority, i);
 		}
+		else if(priorityValue == -3){
+			printTime();
+			printf("O valor recebido na linha %d não é um valor de prioridade válido (de 0 a 7).\n", i);
+		}
+		else if(priorityValue == -4){
+			printTime();
+			printf("O programa %s referenciado na linha %d não foi encontrado. A linha será ignorada.\n", program, i);
+		}
 		else{
-			priorityValue = (int) priority[9] - '0';
-			if(priorityValue > 7 || priorityValue < 0){
-				printf("O valor %d não é um valor de prioridade válido (de 0 a 7).\n", priorityValue);
-			}
-			else{
-				printf("O programa %s teve sintaxe válida e entrará na lista de novos programas com prioridade %d.\n", program, priorityValue);
-				strcpy(newProgramsList[programCount], program);
-				priorityList[programCount] = priorityValue;
-				programCount++;
-			}
+			printTime();
+			printf("O programa %s teve sintaxe válida e entrará na lista de novos programas com prioridade %d.\n", program, priorityValue);
+			strcpy(newProgramsList[programCount], program);
+			priorityList[programCount] = priorityValue;
+			programCount++;
 		}
 		i++;
 	}
@@ -73,8 +113,15 @@ int main(int argc, char const *argv[])
 	for(i=0;i<programCount;i++){
 		printf("%s %d\n", newProgramsList[i], priorityList[i]);
 	}
-	printf("Enviando programas ao escalonador de prioridade...\n");
-	//priorityScheduler(newProgramsList, programCount);
+	if (programCount != 0){
+		printTime();
+		printf("Enviando programas ao escalonador round-robin..\n");
+		//priorityScheduler(newProgramsList, programCount);
+	}
+	else{
+		printTime();
+		printf("Nenhuma entrada válida foi detecatada. O escalonador não será acionado.\n");
+	}
 	fclose(output); 
 	return 0;
 }
--- T1/RoundRobinInterpreter.c
+++ T1/RoundRobinInterpreter.c
@@ -2,32 +2,59 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+
 
 #define PROGRAM_LIMIT 50// Suporta uma entrada de até 50 programas com sintaxe VÁLIDA
 #define CHAR_LIMIT 50
 
-
-//TODO: FAZER CHECAGEM DE INTEGRIDADE EM FUNÇÕES SEPARADAS
-//TODO: FAZER CHECAGEM DE EXISTÊNCIA DOS PROGRAMAS PARA TESTE
 //TODO: PASSAR AS COISAS POR MEMÓRIA COMPARTILHADA
 //TODO: SINCRONIZAR ESCALONADOR E INTERPRETADOR POR SEMÁFORO OU SIGNAL
+
+//TODO: PASSAR AS FUNÇÕES AUXILIARES PARA UM UTIL.H
+
+void printTime(){
+	time_t rawtime;
+  	struct tm * timeinfo;
+
+  	time ( &rawtime );
+  	timeinfo = localtime ( &rawtime );
+  	printf ( "[%d:%d:%d] ", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+}
+
+// Checa a integridade do comando de entrada
+int integrityCheck(char * command, char * program){
+	struct stat programCheck;
+	if(strcmp(command, "exec") != 0){
+		return -1;
+	}
+	if(!(stat(program, &programCheck) == 0 && programCheck.st_mode & S_IXUSR)) {
+		return -4;
+	}
+}
 
 int main(int argc, char const *argv[])
 {
 	char command[CHAR_LIMIT], program[CHAR_LIMIT];
 	char * newProgramsList[PROGRAM_LIMIT];
-	int programCount=0, i;
+	int programCount=0, integrityValue, i;
 	FILE *input, *output;
+
+	printTime();
 	printf("Interpretador Round-Robin:\n");
 	// A entrada e a saída serão feitas por arquivo,
 	// de modo a facilitar a avaliação e teste.
 	input = fopen("input_robin.txt", "r");
 	if(input == NULL){
+		printTime();
 		printf("Erro na abertura do arquivo de entrada.\n");
 		exit(1);
 	}
 	output = fopen("output_robin.txt", "w");
 	if(output == NULL){
+		printTime();
 		printf("Erro na abertura do arquivo de saída.\n");
 		exit(1);
 	}
@@ -38,25 +65,40 @@ int main(int argc, char const *argv[])
 	for(i = 0; i < PROGRAM_LIMIT; i++){
 		newProgramsList[i] = (char *) malloc(CHAR_LIMIT * sizeof(char));
 	}
-
+	i = 0;
 	//Leitura dos comandos
 	while(fscanf(input, "%s %s", command, program)  == 2){
-		if(strcmp(command, "exec") != 0){
-			printf("O comando %s na linha %d não é reconhecido como um comando válido. A linha será ignorada.\n", command, programCount);
+		integrityValue = integrityCheck(command, program);
+		if(integrityValue == -1){
+			printTime();
+			printf("O comando %s na linha %d não é reconhecido como um comando válido. A linha será ignorada.\n", command, i);
+		}
+		else if(integrityValue == -4){
+			printTime();
+			printf("O programa %s referenciado na linha %d não foi encontrado. A linha será ignorada.\n", program, i);
 		}
 		else{
+			printTime();
 			printf("O programa %s teve sintaxe válida e entrará na lista de novos programas\n", program);
 			strcpy(newProgramsList[programCount], program);
 			programCount++;
 		}
+		i++;
 	}
 
 	for(i=0;i<programCount;i++){
 		printf("%s\n", newProgramsList[i]);
 	}
 	fclose(input);
-	printf("Enviando programas ao escalonador round-robin..\n");
-	//roundRobinScheduler(newProgramsList, programCount);
+	if (programCount != 0){
+		printTime();
+		printf("Enviando programas ao escalonador round-robin..\n");
+		//roundRobinScheduler(newProgramsList, programCount);
+	}
+	else{
+		printTime();
+		printf("Nenhuma entrada válida foi detecatada. O escalonador não será acionado.\n");
+	}
 	fclose(output); 
 	return 0;
 }
